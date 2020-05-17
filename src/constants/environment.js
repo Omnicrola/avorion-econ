6e56a6cc87ac89fa3c54a67e359dfec7e7f607f6
Eric Schreffler <omnicrola@gmail.com>
2020-05-17 16:20:04 -0400

    adjusted production icon path

--- src/constants/environment.js
+++ src/constants/environment.js
@@ -3,7 +3,7 @@ const DEV = {
 };
 
 const PROD = {
-    IMG_ROOT: '/avorion-econ/static/images/'
+    IMG_ROOT: '/avorion-econ/images/'
 };
 
 export const ENV = process.env.NODE_ENV === 'production' ? PROD : DEV;
