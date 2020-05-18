0352789a9acdfbe0d0cfe7d3a2b954506ff3a45f
Eric Schreffler <omnicrola@gmail.com>
2020-05-18 11:12:52 -0400

    added custom favicon, updated page title. Tweaked icon borders so icon doesnt bleed into the background

--- src/components/GameIcon.jsx
+++ src/components/GameIcon.jsx
@@ -2,5 +2,9 @@ import React from 'react';
 import {ENV} from '../constants/environment';
 
 export const GameIcon = ({src, size = 'med'}) => {
-    return (<div className={`game-icon icon-size-${size}`}><img src={ENV.IMG_ROOT + "icons/" + src}/></div>);
+    return (<div className={`game-icon icon-size-${size}`}>
+        <div className="inner">
+            <img src={ENV.IMG_ROOT + "icons/" + src}/>
+        </div>
+    </div>);
 };
